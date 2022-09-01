4c4f4fbb136d39fb1133ed8b3991ba6915980bc5
Jonas Læborg <jonas@sharksmedia.dk>
2022-09-01 08:25:16 +0200

    select label type and own agreement

--- catalog/language/da-dk/extension/shipping/shipmondo.php
+++ catalog/language/da-dk/extension/shipping/shipmondo.php
@@ -7,6 +7,7 @@ $_['text_select_pup']       = 'Vælg udleveringsssted';
 $_['text_results']          = '%s udleveringssteder fundet';
 $_['text_order']            = 'Ordrenr. %s';
 $_['text_created']          = 'Forsendelse eksporteret til Shipmondo (ID: %s)';
+$_['text_tracking_number']  = 'Forsendelse eksporteret til Shipmondo (Trackingnr.: %s)';
 
 // Error
 $_['error_address']         = 'Indtast adresse for at vælge udleveringsssted';
--- catalog/language/en-gb/extension/shipping/shipmondo.php
+++ catalog/language/en-gb/extension/shipping/shipmondo.php
@@ -7,6 +7,7 @@ $_['text_select_pup']       = 'Choose Pickup Point';
 $_['text_results']          = '%s pickup points found';
 $_['text_order']            = 'Order ID: %s';
 $_['text_created']          = 'Shipment exported to Shipmondo (ID: %s)';
+$_['text_tracking_number']  = 'Shipment exported to Shipmondo (Tracking: %s)';
 
 // Error
 $_['error_address']         = 'Provide your address to choose preferred pickup point';
